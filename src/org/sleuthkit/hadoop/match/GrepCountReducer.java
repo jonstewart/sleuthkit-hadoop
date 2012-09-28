3a7ac68542163cfca5c7b3b6967b7a11f0a459c1
Joel Uckelman <uckelman@nomic.net>
2012-09-28 12:47:59 +0200

    2-space indent.

--- src/org/sleuthkit/hadoop/match/GrepCountReducer.java
+++ src/org/sleuthkit/hadoop/match/GrepCountReducer.java
@@ -29,35 +29,34 @@ import org.json.simple.JSONObject;
  * Outputs that data to a JSON array. This is then packaged into an output report.
  */
 public class GrepCountReducer extends Reducer<LongWritable, LongWritable, NullWritable, Text> {
+  JSONArray outArray = new JSONArray();
+  String regexes[];
 
-    JSONArray outArray = new JSONArray();
-    String regexes[];
+  @Override
+  protected void setup(Context context) {
+    // TODO: We could make sure that we only have one instance of this
+    // here, since that's all I'm supporting at the moment.
+    regexes = context.getConfiguration().get("mapred.mapper.regex").split("\n");
+  }
 
-    @Override
-    protected void setup(Context context) {
-        // TODO: We could make sure that we only have one instance of this
-        // here, since that's all I'm supporting at the moment.
-        regexes = context.getConfiguration().get("mapred.mapper.regex").split("\n");
+  @SuppressWarnings("unchecked")
+  @Override 
+  protected void reduce(LongWritable key, Iterable<LongWritable> values, Context context) {
+    JSONObject obj = new JSONObject();
+    int sum = 0;
+    for (LongWritable value : values) {
+      sum += value.get();
     }
+    obj.put("a", key.get());
+    obj.put("n", sum);
+    obj.put("kw", regexes[(int) key.get()]);
 
-    @SuppressWarnings("unchecked")
-    @Override 
-    protected void reduce(LongWritable key, Iterable<LongWritable> values, Context context) {
-        JSONObject obj = new JSONObject();
-        int sum = 0;
-        for (LongWritable value : values) {
-            sum += value.get();
-        }
-        obj.put("a", key.get());
-        obj.put("n", sum);
-        obj.put("kw", regexes[(int) key.get()]);
+    outArray.add(obj);
+  }
 
-        outArray.add(obj);
-    }
-
-    @Override
-    protected void cleanup(Context context)
-                                     throws IOException, InterruptedException {
-        context.write(NullWritable.get(), new Text(outArray.toJSONString()));
-    }
+  @Override
+  protected void cleanup(Context context)
+                                   throws IOException, InterruptedException {
+    context.write(NullWritable.get(), new Text(outArray.toJSONString()));
+  }
 }
