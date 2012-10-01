db38eb1ee7fc44f66f5d485a6eb84bbfa58fec77
Joel Uckelman <uckelman@nomic.net>
2012-10-01 15:18:11 +0200

    Typo and clean up /texaspete/ev, also.

--- scripts/tp-clean.sh
+++ scripts/tp-clean.sh
@@ -2,4 +2,5 @@
 
 echo "disable 'entries'; disable 'hash'; disable 'images'; drop 'entries'; drop 'hash'; drop 'images'" | hbase shell
 
-hadoop fs rm -r -f '/texaspete/data/*'
+hadoop fs -rm -r -f '/texaspete/data/*'
+hadoop fs -rm -r -f '/texaspete/ev/*'
